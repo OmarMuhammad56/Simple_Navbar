8cec29f920e3587fbb091194412543392b5e0040
OmarMuhammad56 <omarujan56@gmail.com>
2024-10-11 14:30:03 +0500

    updated code

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
  const Hero = () => {
   return (
@@ -7,9 +8,15 @@ import Image from 'next/image'
     
     <Image src="/images/cvbackgroundimage1.jpg" alt="Background Image" height={800} width={800} className='w-full h-full'>
       </Image>  
-     <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/90 flex flex-col gap-y-8 md:gap-y-0 md:flex-row"></div> 
-     <div className="flex flex-col ml-[15px] md:ml-[80px]"></div>     
-     <h1 className='text-white text-3xl md:text-[85px] leading-9 font-medium'></h1> 
+     <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/90 flex flex-col gap-y-8 md:gap-y-0 md:flex-row"> 
+     <div className="flex flex-col ml-[15px] md:ml-[80px]">    
+  
+     </div>
+     </div> 
+     <div>
+      <Link href="/https://www.linkedin.com/in/omar-muhammad-00073328b/"></Link>
+      <Image src="/images/omar.png" alt="Omar Image" height={400} width={400} ></Image>
+     </div>
    </section>
   )
 }
